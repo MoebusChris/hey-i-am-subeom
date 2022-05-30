204f83213273811d10e56e1679822c6735c708d7
Subeom Jeong <subeomjeong@Subeoms-MBP.attlocal.net>
2022-05-29 18:45:28 -0700

    finished community template

--- src/components/community/index.js
+++ src/components/community/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Community = () => {
   return (
-    <div className="py-20 max-w-7xl mx-auto">
+    <div className="py-20 max-w-5xl mx-auto">
       <div>
         <div>
           <h2 className="text-center font-brandon mb-8 text-2xl md:text-4xl font-bold">
@@ -11,36 +11,45 @@ const Community = () => {
         </div>
       </div>
 
-      <div className="flex flex-col justify-center items-center">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {/*Card-1 */}
-        <div className="w-80 h-48 mb-4">
-          <div className="p-8">
-            <h4>Free Coding Camp</h4>
-            <p>
+        <div className="flex justify-center">
+          <div className="max-w-xs p-8 border border-slate-400">
+            <h4 className="text-2xl font-semibold font-brandon mb-2">
+              Free Coding Camp
+            </h4>
+            <p className="font-roboto">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem a
               culpa natus veritatis, facilis dolor quasi corporis tempora esse
             </p>
           </div>
         </div>
         {/*Card-2 */}
-
-        <div className="w-80 h-48 mb-4">
-          <div className="p-8">
-            <h4>Free Coding Camp</h4>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem a
-              culpa natus veritatis, facilis dolor quasi corporis tempora esse
-            </p>
+        <div className="flex justify-center">
+          <div className="max-w-xs p-8 border border-slate-400">
+            <div>
+              <h4 className="text-2xl font-semibold font-brandon mb-2">
+                Free Coding Camp
+              </h4>
+              <p className="font-roboto">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem a
+                culpa natus veritatis, facilis dolor quasi corporis tempora esse
+              </p>
+            </div>
           </div>
         </div>
         {/*Card-3 */}
-        <div className="w-80 h-48 mb-4">
-          <div className="p-8">
-            <h4>Free Coding Camp</h4>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem a
-              culpa natus veritatis, facilis dolor quasi corporis tempora esse
-            </p>
+        <div className="flex justify-center">
+          <div className="max-w-xs p-8 border border-slate-400">
+            <div>
+              <h4 className="text-2xl font-semibold font-brandon mb-2">
+                Free Coding Camp
+              </h4>
+              <p className="font-roboto">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem a
+                culpa natus veritatis, facilis dolor quasi corporis tempora esse
+              </p>
+            </div>
           </div>
         </div>
       </div>
