3d1bd27a05af4836381227441b4433d3807c9706
Subeom Jeong <subeomjeong@Subeoms-MBP.attlocal.net>
2022-06-18 14:36:24 -0700

    comm card to 9

--- src/components/Community/index.js
+++ src/components/Community/index.js
@@ -9,11 +9,17 @@ const Community = () => {
           <div>
             <h2 className="black font-brandon">My community</h2>
           </div>
+          <div>
+            <p>
+              I share the communities and forums that I follow to acquire more
+              knowledge and ideas about future finance industry.
+            </p>
+          </div>
         </div>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {/*Card-1 */}
+        {/*Card-1 starts here */}
         <div className="flex justify-center">
           <div className="max-w-xs p-8 border border-slate-400">
             <h4 className="text-2xl font-semibold font-brandon mb-2">
@@ -25,7 +31,8 @@ const Community = () => {
             </p>
           </div>
         </div>
-        {/*Card-2 */}
+        {/*Card-1 ends here */}
+        {/*Card-2 starts here */}
         <div className="flex justify-center">
           <div className="max-w-xs p-8 border border-slate-400">
             <div>
@@ -39,7 +46,8 @@ const Community = () => {
             </div>
           </div>
         </div>
-        {/*Card-3 */}
+        {/*Card-2 ends here*/}
+        {/*Card-3 ends here */}
         <div className="flex justify-center">
           <div className="max-w-xs p-8 border border-slate-400">
             <div>
@@ -53,6 +61,85 @@ const Community = () => {
             </div>
           </div>
         </div>
+        {/*Card-3 ends here */}
+        {/*Card-4 starts here */}
+        <div className="flex justify-center">
+          <div className="max-w-xs p-8 border border-slate-400">
+            <h4 className="text-2xl font-semibold font-brandon mb-2">
+              Free Coding Camp
+            </h4>
+            <p className="font-roboto">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem a
+              culpa natus veritatis, facilis dolor quasi corporis tempora esse
+            </p>
+          </div>
+        </div>
+        {/*Card-4 ends here */}
+        {/*Card-5 starts here */}
+        <div className="flex justify-center">
+          <div className="max-w-xs p-8 border border-slate-400">
+            <h4 className="text-2xl font-semibold font-brandon mb-2">
+              Free Coding Camp
+            </h4>
+            <p className="font-roboto">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem a
+              culpa natus veritatis, facilis dolor quasi corporis tempora esse
+            </p>
+          </div>
+        </div>
+        {/*Card-5 ends here */}
+        {/*Card-6 starts here */}
+        <div className="flex justify-center">
+          <div className="max-w-xs p-8 border border-slate-400">
+            <h4 className="text-2xl font-semibold font-brandon mb-2">
+              Free Coding Camp
+            </h4>
+            <p className="font-roboto">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem a
+              culpa natus veritatis, facilis dolor quasi corporis tempora esse
+            </p>
+          </div>
+        </div>
+        {/*Card-6 ends here */}
+        {/*Card-7 starts here */}
+        <div className="flex justify-center">
+          <div className="max-w-xs p-8 border border-slate-400">
+            <h4 className="text-2xl font-semibold font-brandon mb-2">
+              Free Coding Camp
+            </h4>
+            <p className="font-roboto">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem a
+              culpa natus veritatis, facilis dolor quasi corporis tempora esse
+            </p>
+          </div>
+        </div>
+        {/*Card-7 ends here */}
+        {/*Card-8 starts here */}
+        <div className="flex justify-center">
+          <div className="max-w-xs p-8 border border-slate-400">
+            <h4 className="text-2xl font-semibold font-brandon mb-2">
+              Free Coding Camp
+            </h4>
+            <p className="font-roboto">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem a
+              culpa natus veritatis, facilis dolor quasi corporis tempora esse
+            </p>
+          </div>
+        </div>
+        {/*Card-8 ends here */}
+        {/*Card-9 starts here */}
+        <div className="flex justify-center">
+          <div className="max-w-xs p-8 border border-slate-400">
+            <h4 className="text-2xl font-semibold font-brandon mb-2">
+              Free Coding Camp
+            </h4>
+            <p className="font-roboto">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem a
+              culpa natus veritatis, facilis dolor quasi corporis tempora esse
+            </p>
+          </div>
+        </div>
+        {/*Card-9 ends here */}
       </div>
     </div>
   )
