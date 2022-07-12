180153c1fca2e7e6fba24a7205c4ccc579d50020
Subeom Jeong <subeomjeong@Subeoms-MBP.attlocal.net>
2022-07-11 21:47:45 -0700

    trimming journey component wip

--- src/components/Journey/index.js
+++ src/components/Journey/index.js
@@ -12,166 +12,205 @@ const Journey = () => {
           </div>
         </div>
         {/*Trinion America here */}
-        <div class="max-w-md mx-auto bg-white overflow-hidden md:max-w-4xl">
-          <div class="md:flex">
-            <div class="flex justify-center p-8 md:shrink-0">
+        <div className="max-w-md mx-auto bg-white overflow-hidden md:max-w-4xl">
+          <div className="md:flex">
+            <div className="flex justify-center p-8 md:shrink-0">
               <StaticImage
                 src="https://i0.wp.com/illinoisksa.org/wp-content/uploads/kboard_attached/7/202112/61b3bd1b52ed95971902.jpg?w=685&ssl=1"
                 alt="Trinion"
                 className="w-96 h-80 md:w-44 md:h-[138px] bg-top rounded-3xl self-center"
               />
             </div>
-            <div class="p-8">
+            <div className="p-8">
               <a
                 href="#"
-                class="capitalize tracking-wide text-xl md:text-2xl text-sky-600 font-normal font-brandon  hover:underline"
+                className="capitalize tracking-wide text-xl md:text-2xl text-sky-600 font-normal font-brandon  hover:underline"
               >
                 trinion america, inc.
               </a>
-              <div class="block mt-2 text-base leading-tight font-medium text-lime-700 font-roboto break-normal">
+              <div className="block mt-2 text-base leading-tight font-medium text-lime-700 font-roboto break-normal">
                 <p className="italic">
                   Financial Accounting Specialist, Since February 2020
                 </p>
               </div>
-              <div class="block mt-2 text-base leading-tight font-medium text-slate-600 font-roboto">
+              <div className="block mt-2 text-base leading-tight font-medium text-slate-600 font-roboto">
                 San Diego, CA
               </div>
-              <p class="mt-2 text-slate-500 font-roboto">
-                It is grateful to have an opportunity to work at a Hyundai
-                Motors Affiliate, Trinion America. Trinion America Inc. provides
-                transporting services and warehouse management services. Working
-                in the accounting team, I prepare financial statements and
-                reports, advising the top management on sales practices and
-                strategies. As the business grows fast, the team I work with
-                focuses on strategic analysis for more profitable acquisitions.
-                In several projects, my role is to break down the costs and
-                expenses, measure future cash flow projections, and estimate an
-                acceptable rate of return for acquisition. I am detail-oriented
-                and numeric-oriented, and these personal traits help me deduce
-                more accurate P&amp;L projections.
-              </p>
+              <div className="mt-2 text-slate-500 font-roboto text-[15px] font-light">
+                <div>
+                  <p className="mb-4">
+                    It is grateful to have an opportunity to work at a Hyundai
+                    Motors Affiliate, Trinion America. Trinion America Inc.
+                    provides transporting services and warehouse management
+                    services.
+                  </p>
+                  <p className="mb-4">
+                    I joined the HR team after 3 months temporary position. As a
+                    HR specialist, I experienced many aspect of business
+                    operation, and
+                  </p>
+                  <p className="mb-4">
+                    Landing finance position, all I learned in college begins
+                    shining. In financial accounting team, I helped two CPAs to
+                    prepare financial statement, plan budget, and help
+                    management make smarter decision. As the business grows
+                    fast, the team I work with also participate in several
+                    projects for new businesses and company acquisition. My team
+                    is to break down the costs and expenses, measure future cash
+                    flow projections, and estimate an acceptable rate of return
+                    for acquisition.
+                  </p>
+
+                  <p className="mb-4">
+                    I handle financial data everyday, and it is painful to
+                    analyze a problem with a spreadsheet and my gut instincts.
+                    That is where my curiosity and everyday-learner mind set are
+                    kicked in. I was looking for a way to grow my full-stack
+                    development experience, and data analytics will be it.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
         {/*Blue Ocean here */}
-        <div class="max-w-md mx-auto bg-white overflow-hidden md:max-w-4xl">
-          <div class="md:flex">
-            <div class="flex justify-center p-8 md:shrink-0">
+        <div className="max-w-md mx-auto bg-white overflow-hidden md:max-w-4xl">
+          <div className="md:flex">
+            <div className="flex justify-center p-8 md:shrink-0">
               <StaticImage
                 src="https://images.squarespace-cdn.com/content/v1/58c5cd72bf629a853368b4eb/1493105380503-CUFZ4ULOK7OO3C64XIES/Signage_all.jpg?format=1500w"
                 alt="Blue Ocean"
                 className="w-96 h-80 md:w-44 md:h-[138px] bg-top rounded-3xl self-center"
               />
             </div>
-            <div class="p-8">
+            <div className="p-8">
               <a
                 href="#"
-                class="capitalize tracking-wide text-xl md:text-2xl text-sky-600 font-normal font-brandon  hover:underline"
+                className="capitalize tracking-wide text-xl md:text-2xl text-sky-600 font-normal font-brandon  hover:underline"
               >
                 Blue Ocean Sushi
               </a>
-              <div class="block mt-2 text-base leading-tight font-medium text-lime-700 font-roboto break-normal">
+              <div className="block mt-2 text-base leading-tight font-medium text-lime-700 font-roboto break-normal">
                 <p className="italic">
                   General Manager, October 2016-February 2020
                 </p>
               </div>
-              <div class="block mt-2 text-base leading-tight font-medium text-slate-600 font-roboto">
+              <div className="block mt-2 text-base leading-tight font-medium text-slate-600 font-roboto">
                 Carlsbad, CA
               </div>
-              <p class="mt-2 text-slate-500 font-roboto">
-                I have been in the food industry before pursuing my financial
-                career. I had been working and studying in my entire college
-                year. I chose to work at a restaurant because of the flexible
-                work schedules. A restaurant is not a simple place as many
-                thought it was. I would arguably say that restaurant jobs are
-                quite fast-paced and stressful. I have witnessed many restaurant
-                workers fail to manage their anger and anxiety at work. However,
-                I took my job differently and ceased criticizing what I was
-                doing and what I would have done. Instead, I decided to be
-                grateful, diligent, and reliable. I established good
-                relationships with my co-workers and engaged with customers. I
-                never dropped smiles when I took care of my customers. It was
-                not to make more money out of gratuity but to establish the
-                business mind. After all, I had been promoted three times in the
-                first six months and became a general manager at Blue Ocean
-                Sushi group, operating 7 restaurants in San Diego, CA.
-              </p>
+              <div className="mt-2 text-slate-500 font-roboto text-[15px] font-light">
+                <p className="mb-4">
+                  Blue Ocean sushi is where I get the 'Rise and Grind'
+                  mentality. The rise and grind is not healthy. At all. But, it
+                  definitely makes you stronger.
+                </p>
+                <p className="mb-4">
+                  I had been working and studying in my entire college year.
+                  Coming from working class family, financial struggle is always
+                  an obstacle. I had to work harder to pay tuition and to live
+                  off. I took lectures in school until afternoon and worked
+                  until midnight, then studied class materials until morning.
+                  This routine had been going on years.
+                </p>
+                <p className="mb-4">
+                  I worked hard, established good relationships with my
+                  co-workers, and engaged with customers. I eventually
+                  understood the importance of relationship with team members
+                  and customers. My name began to appear on Blue Ocean's Yelp
+                  page shortly. I promoted 3 times in half years, then the owner
+                  offered me general manager position.
+                </p>
+                <p className="mb-4">
+                  The tears and pain when I worked at Blue Ocean, left in my
+                  mentality, become armor to endure.
+                </p>
+              </div>
             </div>
           </div>
         </div>
         {/*UCSD here */}
-        <div class="max-w-md mx-auto bg-white overflow-hidden md:max-w-4xl">
-          <div class="md:flex">
-            <div class="flex justify-center p-8 md:shrink-0">
+        <div className="max-w-md mx-auto bg-white overflow-hidden md:max-w-4xl">
+          <div className="md:flex">
+            <div className="flex justify-center p-8 md:shrink-0">
               <StaticImage
                 src="https://2qqce331qbpvuwhs03ipa6o4-wpengine.netdna-ssl.com/wp-content/uploads/2016/10/cover.jpg"
                 alt="Giesel Library"
                 className="w-96 h-80 md:w-44 md:h-[138px] bg-top rounded-3xl self-center"
               />
             </div>
-            <div class="p-8">
+            <div className="p-8">
               <a
                 href="#"
-                class="capitalize tracking-wide text-xl md:text-2xl text-sky-600 font-normal font-brandon  hover:underline"
+                className="capitalize tracking-wide text-xl md:text-2xl text-sky-600 font-normal font-brandon  hover:underline"
               >
                 University of California, San Diego
               </a>
-              <div class="block mt-2 text-base leading-tight font-medium text-lime-700 font-roboto break-normal">
+              <div className="block mt-2 text-base leading-tight font-medium text-lime-700 font-roboto break-normal">
                 <p className="italic"> B.A. in Economics, earned Dec 2022</p>
               </div>
-              <div class="block mt-2 text-base leading-tight font-medium text-slate-600 font-roboto">
+              <div className="block mt-2 text-base leading-tight font-medium text-slate-600 font-roboto">
                 La Jolla, CA
               </div>
-              <p class="mt-2 text-slate-500 font-roboto">
-                UC San Diego presented a variety of exciting, engaging, and
-                diverse cultures. My college year at UCSD offered an opportunity
-                of engaging with phenomenal faculty and peers. My experience
-                with professors integrated the abstract concepts of economy and
-                finance, turning out to be a concrete understanding of business
-                and investment. I enjoyed peer-to-peer practice outside of
-                lectures and supplementary activities to improve my insight.
-                Professors liked to be challenged by students to share thoughts
-                and ideas. The faculty did not only focus on students' academic
-                attainment but also did they emphasize the importance of
-                diversity.
+              <p className="mt-2 text-slate-500 font-roboto text-[15px] font-light">
+                <p className="mb-4">
+                  "Cool breeze down from the ocean and nice folks."
+                </p>
+                <p className="mb-4">It's honor to shout out "I'm a Triton!"</p>
+                <p className="mb-4">
+                  Challenging, It was definitely challenging, but so motivating.
+                </p>
+                <p className="mb-4">
+                  UC San Diego presented a variety of exciting, engaging, and
+                  diverse cultures. My college year at UCSD offered an
+                  opportunity of engaging with phenomenal faculty and peers. My
+                  experience with professors integrated the abstract concepts of
+                  economy and finance, turning out to be a concrete
+                  understanding of business and investment. I enjoyed
+                  peer-to-peer practice outside of lectures and supplementary
+                  activities to improve my insight. Professors liked to be
+                  challenged by students to share thoughts and ideas.
+                </p>
+                <p>
+                  The experience in UCSD became compass to guide me. The spirit
+                  and philosophy from professors and peers are now a part of me,
+                  not fear to light up.
+                </p>
+                The faculty did not only focus on students' academic attainment
+                but also did they emphasize the importance of diversity.
               </p>
             </div>
           </div>
         </div>
         {/*UCLA Boot camp here */}
-        <div class="max-w-md mx-auto bg-white overflow-hidden md:max-w-4xl">
-          <div class="md:flex">
-            <div class="flex justify-center p-8 md:shrink-0">
+        <div className="max-w-md mx-auto bg-white overflow-hidden md:max-w-4xl">
+          <div className="md:flex">
+            <div className="flex justify-center p-8 md:shrink-0">
               <StaticImage
                 src="https://the-winston-project.imgix.net/610beec5aea874132acbafda/126_ucla-extension.png"
                 alt="UCLA Bootcamp"
                 className="w-96 h-80 md:w-44 md:h-[138px] bg-top rounded-3xl self-center"
               />
             </div>
-            <div class="p-8">
+            <div className="p-8">
               <a
                 href="#"
-                class="capitalize tracking-wide text-xl md:text-2xl text-sky-600 font-normal font-brandon  hover:underline"
+                className="capitalize tracking-wide text-xl md:text-2xl text-sky-600 font-normal font-brandon  hover:underline"
               >
-                The coding bootcamp, UCLA extention
+                UCLA Coding Bootcamp
               </a>
-              <div class="block mt-2 text-base leading-tight font-medium text-lime-700 font-roboto break-normal">
+              <div className="block mt-2 text-base leading-tight font-medium text-lime-700 font-roboto break-normal">
                 Programming certificate Course,
                 <p className="italic">September 2021-March 2022</p>
               </div>
-              <div class="block mt-2 text-base leading-tight font-medium text-slate-600 font-roboto">
+              <div className="block mt-2 text-base leading-tight font-medium text-slate-600 font-roboto">
                 Los Angeles, CA
               </div>
-              <p class="mt-2 text-slate-500 font-roboto">
-                I'm so excited to add programming skills to my toolbox. I always
-                follow new IT technologies, and Web3 got my attention. I
-                researched some good places to start. The developer course was
-                the thing. There are always lots of fun stuff to learn in web
-                application development. I enjoyed every corner, and I keep
-                expanding skills and knowledges. Now, I'm learning Data
-                Analytics to integrate my financial insight with my IT
-                knowledges.
-              </p>
+              <div className="mt-2 text-slate-500 font-roboto text-[15px] font-light">
+                I'm so excited to add programming skills to my toolbox. It was a
+                great place to start to learn to code. There are lots of fun
+                stuff to learn in web application development. I enjoyed every
+                corner, and I keep expanding skills and knowledge.
+              </div>
             </div>
           </div>
         </div>
